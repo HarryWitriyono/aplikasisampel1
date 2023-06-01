e61172d3e0b041f521b0f90bcaeb9dc06cd4b847
Harry Witriyono <49424961+HarryWitriyono@users.noreply.github.com>
2023-06-01 07:23:47 +0700

    Update hapusmhs.php

--- hapusmhs.php
+++ hapusmhs.php
@@ -2,7 +2,7 @@
 if (isset($_GET['npm'])) {
 	$npmdihapus=$_GET['npm'];
 	$sql="delete from mahasiswa where npm='".$npmdihapus."'";
-	$kon=mysqli_connect("localhost","root","","aplikasisampel1");
+	include_once('koneksi.db.php');
 	$q=mysqli_query($kon,$sql);
 }
 header('location:mahasiswa.php');
