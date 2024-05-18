c9bca57f081a49928da6f7143d621271114b275a
Harry Witriyono <49424961+HarryWitriyono@users.noreply.github.com>
2024-05-18 10:22:56 +0700

    Add files via upload

--- koneksi.db.php
+++ koneksi.db.php
@@ -1,1 +1,7 @@
-<?php $kon=mysqli_connect("localhost","root","","aplikasisampel1"); ?>
+<?php 
+$host="localhost";
+$user="apksampel1";
+$password="@UmB#16052024#Hw";
+$db="aplikasisampel1";
+$koneksi=mysqli_connect($host,$user,$password,$db) or die('Gagal koneksi database !');
+?>
